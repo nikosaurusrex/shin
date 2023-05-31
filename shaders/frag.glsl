24b013831e0c4537fffd261d9cfe6641e239e3e8
cactulus <Nikolas@Kaipel.net>
2023-05-31 21:21:09 +0200

    removed uint support for macos version

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -1,38 +1,36 @@
 #version 410 core
 
-#extension GL_ARB_explicit_uniform_location : enable
-
 layout(origin_upper_left) in vec4 gl_FragCoord;
 
 layout(location = 0) out vec4 color;
 
 uniform sampler2D glyph_map;
-uniform usampler2D cells;
+uniform isampler2D cells;
 
-uniform uvec2 cell_size;
-uniform uvec2 win_size;
+uniform ivec2 cell_size;
+uniform ivec2 win_size;
 uniform float time;
 
-vec3 unpack_color(uint cp) {
-	uint r = (cp >> 16) & 0xFFu;
-	uint g = (cp >> 8) & 0xFFu;
-	uint b = cp & 0xFFu;
+vec3 unpack_color(int cp) {
+	int r = (cp >> 16) & 0xFF;
+	int g = (cp >> 8) & 0xFF;
+	int b = cp & 0xFF;
 
 	return vec3(r, g, b) / 255.0;
 }
 
 void main() {
-	uvec2 screen_pos = uvec2(floor(gl_FragCoord.xy));
+	ivec2 screen_pos = ivec2(floor(gl_FragCoord.xy));
 
 	vec3 pixel;
 	if (screen_pos.x < win_size.x && screen_pos.y < win_size.y) {
-		uvec2 cell_index = uvec2(floor(screen_pos / cell_size));
-		uvec2 cell_pos = screen_pos % cell_size;
+		ivec2 cell_index = ivec2(floor(screen_pos / cell_size));
+		ivec2 cell_pos = screen_pos % cell_size;
 
-		uvec4 cell = texelFetch(cells, ivec2(cell_index), 0);
+		ivec4 cell = texelFetch(cells, ivec2(cell_index), 0);
 		
-		uint cell_x = cell.x % 32u;
-		uint cell_y = cell.x / 32u;
+		int cell_x = cell.x % 32;
+		int cell_y = cell.x / 32;
 
 		vec4 texel = texelFetch(glyph_map, ivec2(
 			cell_x * cell_size.x + cell_pos.x,
